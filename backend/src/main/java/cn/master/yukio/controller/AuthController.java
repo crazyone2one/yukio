708a4b2855eaf5bef848906cfcdf276d6f5b67fe
jingll <the2ndindec@hotmail.com>
2024-02-21 16:33:50 +0800

    :construction: demo

--- backend/src/main/java/cn/master/yukio/controller/AuthController.java
+++ backend/src/main/java/cn/master/yukio/controller/AuthController.java
@@ -1,23 +1,26 @@
 package cn.master.yukio.controller;
 
 import cn.master.yukio.dto.LoginRequest;
-import cn.master.yukio.entity.User;
 import cn.master.yukio.security.CustomUserDetails;
+import cn.master.yukio.security.CustomUserDetailsService;
 import cn.master.yukio.security.JwtProvider;
 import cn.master.yukio.util.RedisKeyUtils;
+import io.jsonwebtoken.Claims;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +35,9 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtProvider jwtProvider;
     private final RedisTemplate<String, Object> redisTemplate;
+    private final CustomUserDetailsService customUserDetailsService;
+    @Value("${jwt.auto-refresh-ttl}")
+    private long autoRefreshTtl;
 
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody LoginRequest user) {
@@ -48,5 +54,30 @@ public class AuthController {
 
         log.info("{} logged in", customUserDetails.getUser().getName());
         return ResponseEntity.ok(map);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<Object> refresh(@NotBlank(message = "refreshToken不能为空") @RequestParam String refreshToken) {
+        if (jwtProvider.isTokenValid(refreshToken)) {
+            Map<String, Object> map = new HashMap<>();
+            Claims claims = jwtProvider.parseTokenToClaims(refreshToken);
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(claims.getSubject());
+            if ((claims.getExpiration().getTime() - new Date(System.currentTimeMillis()).getTime()) <= autoRefreshTtl) {
+                String newRefreshToken = jwtProvider.createRefreshToken(userDetails);
+                map.put("refreshToken", newRefreshToken);
+                log.info("{}'s refreshToken recreated.", claims.getSubject());
+            }
+            String accessToken = jwtProvider.createAccessToken(userDetails);
+            map.put("token", accessToken);
+            log.info("{}'s token refreshed.", claims.getSubject());
+            return ResponseEntity.ok(map);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("请重新登录");
+        }
+    }
+
+    @GetMapping("/demo")
+    public ResponseEntity<Object> demo() {
+        return ResponseEntity.ok("demo");
     }
 }
